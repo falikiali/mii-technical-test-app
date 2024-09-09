0d8f6557affe8449e04a88ce3c6d6b6c008585b0
Ali Faliki <alifaliki456@gmail.com>
2024-09-09 21:01:57 +0700

    fix: create chart for portofolio

--- app/src/main/java/com/mii/techincaltest/app/presentation/portofolio/PortofolioScreen.kt
+++ app/src/main/java/com/mii/techincaltest/app/presentation/portofolio/PortofolioScreen.kt
@@ -1,14 +1,21 @@
 package com.mii.techincaltest.app.presentation.portofolio
 
+import android.util.Log
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
@@ -17,12 +24,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.mii.techincaltest.app.component.DonutChart
 import com.mii.techincaltest.app.helper.GeneralHelper
 import com.mii.techincaltest.app.helper.GeneralHelper.convertToRupiah
 import com.mii.techincaltest.app.helper.Screens
@@ -32,6 +41,11 @@ fun PortofolioScreen(
     modifier: Modifier,
     navController: NavHostController
 ) {
+    val listPercentages = GeneralHelper.parsingJsonPortofolioToModel().data.map {
+        it.percentage.toFloat() / 100
+    }
+    val listColors = listOf(Color.Red, Color.Blue, Color.Green, Color.Magenta)
+
     Column(
         modifier = modifier
             .fillMaxWidth()
@@ -47,11 +61,21 @@ fun PortofolioScreen(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(
-            text = "Chart portofolio",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Bold,
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.background),
+            contentAlignment = Alignment.Center
+        ) {
+            DonutChart(
+                modifier = Modifier
+                    .height(300.dp)
+                    .align(Alignment.Center)
+                    .fillMaxWidth(),
+                percentages = listPercentages,
+                colors = listColors
+            )
+        }
         
         Spacer(modifier = Modifier.height(16.dp))
         
@@ -74,11 +98,22 @@ fun PortofolioScreen(
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(
+                    Row(
                         modifier = Modifier
                             .padding(16.dp),
-                        text = GeneralHelper.parsingJsonPortofolioToModel().data[it].label
-                    )
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Canvas(modifier = Modifier.size(32.dp), onDraw = {
+                            drawCircle(color = listColors[it])
+                        })
+                        
+                        Spacer(modifier = Modifier.width(16.dp))
+
+                        Text(
+                            modifier = Modifier,
+                            text = GeneralHelper.parsingJsonPortofolioToModel().data[it].label
+                        )
+                    }
 
                     Text(
                         modifier = Modifier
